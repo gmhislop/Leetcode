601a0fc7b8c5753b1de6d3dbedff1b5d9bbb1495
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2022-10-11 22:55:15 +0200

    Time: 283 ms (65.83%), Space: 51.7 MB (18.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,6 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    const reversed = x.toString().split('').reverse().join('')
-    return (x.toString() === reversed);
+    let reversedStr = ''
+    let xStr = x.toString()
+    
+    for (let i of xStr){
+        reversedStr = i + reversedStr
+    }
+    return (reversedStr === xStr)
 };
